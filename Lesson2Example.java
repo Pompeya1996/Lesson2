ea28b0f35c3eab49b20bbc31a57217090944d279
Pompeya1996 <110059513+Pompeya1996@users.noreply.github.com>
2022-07-28 20:51:47 +0200

    Add files via upload

---
+++ Lesson2Example.java
@@ -0,0 +1,27 @@
+public class Lesson2Example {
+    public static void main(String[] args) {
+        int temp;
+        temp = 567754543;
+        System.out.println(temp);
+
+        int value = 26;
+        System.out.println(value);
+        value = temp;
+        System.out.println(value);
+
+        double value2 = 78.123;
+        System.out.println(value2);
+
+        boolean booLeanValue = true;
+        System.out.println(booLeanValue);
+
+        char testValue = 'a';
+        System.out.println(testValue);
+
+        testValue = 'f';
+        System.out.println(testValue);
+
+    }
+
+
+}
